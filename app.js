0cf37b9d8523a218fde66a8382b34852ba544652
ophicial-pauloski <pasukwo5@gmail.com>
2021-04-20 23:07:45 -1100

    changing entire UI to new UI

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var userInput = document.getElementById('userinput');
-var checkBtn = document.getElementById('submitebtn'); 
+var checkBtn = document.getElementById('submitbtn'); 
 var result = document.getElementById('result'); 
 var closePopper = document.getElementById('closebtn'); 
-var popper = document.querySelector('.result-poper');
+var popper = document.querySelector('.popper-layer');
 
 const secretNumber = Math.floor(Math.random() * 6 + 2);
 checkBtn.addEventListener('click', ()=>{
     if (userInput.value.length == 0) {
         popper.classList.add("active");
-        result.textContent = "Input field can't be empty, please type in a number to process";
+        result.textContent = "You didn't type in anything right? Please type in somthing to process";
     }else if (userInput.value != secretNumber) {
     popper.classList.add("active");
     result.textContent = `${userInput.value}  is wrong! please try again ❌🤦‍♂️❌🤦‍♂️The correct answer at this moment is ${secretNumber}
